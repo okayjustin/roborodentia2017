d4201613c890a513a343707e159462252d31c5fa
Justin Ng <okayjustinn@gmail.com>
2016-12-19 06:25:44 -0800

    Fixing commits...

---
+++ arm/lib/gcc/arm-none-eabi/4.9.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../src/gcc-4.9.1/configure --target=arm-none-eabi --prefix=/Users/justinng/Documents/Github/roborodentia2017/arm --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-multilib --with-system-zlib --with-newlib --without-headers --disable-shared --disable-nls --with-gnu-as --with-gnu-ld --enable-languages=c : (reconfigured) /Users/justinng/Documents/Github/roborodentia2017/arm/src/gcc-4.9.1/configure --target=arm-none-eabi --prefix=/Users/justinng/Documents/Github/roborodentia2017/arm --with-cpu=cortex-m4 --with-fpu=fpv4-sp-d16 --with-float=hard --with-mode=thumb --enable-interwork --enable-multilib --enable-languages=c,c++ --with-system-zlib --with-newlib --disable-shared --disable-nls --with-gnu-as --with-gnu-ld";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-m4" }, { "float", "hard" }, { "mode", "thumb" }, { "fpu", "fpv4-sp-d16" } };
